b93f70ac06ff17bcedc2fc58ed2a582208ede160
zou <zouxq412@foxmail.com>
2019-08-07 09:48:40 +0800

    单例模式

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # design-pattern
 设计模式demo
+
+
+
+# 设计模式
+
+单例模式：[单例模式-只有一个实例](https://www.turbosnail.top/blog/design-pattern-singleton/)
+
---
+++ src/main/java/com/turbo/singleton/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 单例模式：
+ * 饿汉式
+ * 懒汉式单例（非线程安全）
+ * 懒汉式单例（synchronized关键字线程安全）
+ * 双重校验锁（double-checked locking，DCL）
+ * 静态内部类
+ * 枚举方式
+ * @author zouxq
+ */
+package com.turbo.singleton;
