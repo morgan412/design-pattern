cb8910e02367cb71ebe2ddf36daddd03a97c44c3
zou <zouxq412@foxmail.com>
2019-08-09 17:55:37 +0800

    建造者模式

---
+++ src/main/java/com/turbo/builder/product/Meal.java
@@ -0,0 +1,13 @@
+package com.turbo.builder.product;
+
+import java.util.List;
+
+/**
+ * 产品类
+ *
+ * @author zouxq
+ */
+public class Meal {
+    private List<String> foods;
+    private List<String> drinks;
+}
